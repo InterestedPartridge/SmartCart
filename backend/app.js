5c29fe554e562907d1c46bfa31818a95982260df
012_Aryan Bichpuria <bichpuriar471@gmail.com>
2023-06-21 17:44:38 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const PORT = process.env.PORT
 
 const errorMiddleware = require("./middleware/error");
 
